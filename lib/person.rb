03a5f558569489ed341166979e551f2361e65308
Alexa <kusteralexa@gmail.com>
2023-01-11 11:36:25 -0500

    commit tree 435a33ea5f71ccd6f38fc7f41760e4e761d820d8

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,32 @@
-# your code goes here
+# require "pry"
+
+class Person 
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene =
+    end
+
+    def clean?
+        self.hygiene > 7
+    end
+    
+    def happy?
+        self.happiness > 7 && 
+    end
+end
+
+
+person1 = Person.new("Lex")
+
+
+# binding.pry
+
+
+
+
